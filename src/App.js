3e447d213c626d38103d85994e12e2a4bd7aa14f
Michal Trabski <michal.trabski@gmail.com>
2020-02-03 15:51:27 +0100

    writing custom hook to reach tasks from firebase

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Header } from "./components/layout/Header";
 import { Sidebar } from "./components/layout/Sidebar";
 // import { Content } from "./components/layout/Content";
 import logo from "./logo.svg";
 import "./App.css";
 
-export const App = () => (
-  <div className="App">
-    ITAKA - CRM, raporty
-    <div>
-      <Header />
-      <Sidebar />
+export const App = () => {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    console.log(tasks);
+  }, []);
+
+  return (
+    <div className="App">
+      ITAKA - CRM, raporty
+      <div>
+        <Header />
+        <Sidebar />
+      </div>
     </div>
-  </div>
-);
+  );
+};
